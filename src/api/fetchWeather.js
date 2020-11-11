4f1ef3b47c6fdf20f0103f3e09eaf12fd72fccd9
girish5f9@sasi.ac.in <girish5f9@sasi.ac.in>
2020-11-12 00:18:15 +0530

    gkproject2

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
-const API_KEY = 'YOUR_API_KEY';
+const API_KEY = '411cdd541d2d6358591ee590d152156f';
 
 export const fetchWeather = async (query) => {
     const { data } = await axios.get(URL, {
